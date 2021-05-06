48f04cbcd25048a5a3a25962758a91ddb0460475
Queenshiba <haruka0111.2a@gmail.com>
2021-05-06 20:17:17 +0900

    add loading img function

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+const images = document.querySelectorAll('img')
+
+images.forEach(image => {
+    const newURL = image.getAttribute('data-src')
+    image.src = newURL;
+})
+
+const lpImage = document.getElementById('lp-container-laptop')
+
+// images.forEach(image => {
+    const newURL = image.getAttribute('data-src')
+    lpImage = newURL;
+// })
