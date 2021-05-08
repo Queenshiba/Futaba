506f9ed6018c2b134048a09e712dfee597a3f291
Queenshiba <haruka0111.2a@gmail.com>
2021-05-08 21:45:07 +0900

    made lp responsive

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,13 @@
-const images = document.querySelectorAll('img')
-
-images.forEach(image => {
-    const newURL = image.getAttribute('data-src')
-    image.src = newURL;
-})
-
-const lpImage = document.getElementById('lp-container-laptop')
+// const images = document.querySelectorAll('img')
 
 // images.forEach(image => {
-    const newURL = image.getAttribute('data-src')
-    lpImage = newURL;
+//     const newURL = image.getAttribute('data-src')
+//     image.src = newURL;
 // })
+
+// const lpImage = document.getElementById('lp-container-laptop')
+
+// // images.forEach(image => {
+//     const newURL = image.getAttribute('data-src')
+//     lpImage = newURL;
+// // })
